5d4a63c70b0ce341ad24d9c575f2ed3374d1c687
Feilong Huang <felixh6888@gmail.com>
2019-01-17 19:18:00 -0500

    app comleted

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,15 +3,33 @@ var router = express.Router();
 var path = require("path");
 var friendsArray = require("../data/friends.js")
 router.post("/api/friends", function (req, res) {
-  //save the ajax post data to friendsArray
-  friendsArray.push(req.body);
-  //Here should be the function to compare the recent input with data 
-  //saved in friendsArray,and find the best match one ,then use res.json to 
-  //send the object back,and the function in the ajax post will use this 
-  //data to show the best match one's name and photo.
-  //Here I just send back the curent added one's data back, so it show the curent one's name.
-  res.send(req.body);
   console.log(req.body);
+  var totalDifference = 0;
+  var totalDifferenceArray = [];
+  var newUserDataArray = req.body.scores;
+  console.log("newUserDataArray:" + newUserDataArray);
+  //loop over the friendsArray,put all totaldifference in an array
+  for (var a = 0; a < friendsArray.length; a++) {
+
+    for (var i = 0; i < newUserDataArray.length; i++) {
+      if (newUserDataArray[i] > friendsArray[a].scores[i]) {
+        totalDifference += newUserDataArray[i] - friendsArray[a].scores[i];
+      } else {
+        totalDifference += friendsArray[a].scores[i] - newUserDataArray[i];
+      }
+    }
+    totalDifferenceArray.push(totalDifference);
+    totalDifference = 0;
+  }
+  //find the minimun totaldifference and its index
+  console.log("totalDifferenceArray:" + totalDifferenceArray);
+  var minTotalDifference = Math.min.apply(Math, totalDifferenceArray);
+  console.log("minTotalDifference:" + minTotalDifference);
+  var indexOfBestMatch = totalDifferenceArray.indexOf(minTotalDifference);
+  //send the best match friend back to user
+  res.json(friendsArray[indexOfBestMatch]);
+  friendsArray.push(req.body);
+
 });
 router.get("/api/friends", function (req, res) {
   res.json(friendsArray);
